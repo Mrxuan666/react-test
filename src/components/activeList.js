b2bca6aee231cb2f923b93fd0dc19998b8075d59
Mrxuan6 <985073497@qq.com>
2017-09-20 00:09:10 +0800

    third

--- src/components/activeList.js
+++ src/components/activeList.js
@@ -8,11 +8,8 @@ export default class activeList extends Component{
    render(){
       return(
           <div>
-<<<<<<< HEAD
+
              第二次修改666冲突
-=======
-             第二次修改666冲突
->>>>>>> dev
           </div>
       )
    }
